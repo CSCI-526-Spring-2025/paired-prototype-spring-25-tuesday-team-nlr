8a6eaefd56157847a7618abb36756007c2d85041
Linda Tam <lindatam@usc.edu>
2025-02-04 19:41:23 -0800

    Updated Start/Restart button text

--- Assets/Scripts/MainButton.cs
+++ Assets/Scripts/MainButton.cs
@@ -7,20 +7,31 @@ public class MainButton : MonoBehaviour
 
     public TMP_Text ButtonText;
     private bool isStart = true;
+    private string endText = "Restart";
     // Start is called before the first frame update
     public void OnClick()
     {
         if (isStart)
         {
-            ButtonText.text = "Restart";
+            ButtonText.text = endText;
             isStart = false;
+            StartGame();
+            Debug.Log("Starting");
         }
         else
         {
             RestartGame();
+            isStart = true;
+            ButtonText.text = "Start";
+            Debug.Log("Restart");
         }
     }
     public void RestartGame()
+    {
+
+    }
+
+    public void StartGame()
     {
 
     }
